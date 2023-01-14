42d4fc31e88a3f12d5e26a8a15a002ba5df7d13b
William Fecteau <william.fecteau.1@ulaval.ca>
2023-01-14 09:02:03 -0500

    v2

--- bot.py
+++ bot.py
@@ -6,18 +6,59 @@ class Bot:
     def __init__(self):
         print("Initializing your super mega duper bot")
 
-    def get_next_move(self, game_message: GameMessage):
+    def get_next_move(self, gameMsg: GameMessage):
         """
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
+        self.gameMsg = gameMsg
 
-        other_team_ids = [team for team in game_message.teams if team != game_message.teamId]
+        ourTeamId = gameMsg.teamId
+        otherTeamIds = [team for team in gameMsg.teams if team != gameMsg.teamId]
         actions = list()
+   
 
-        actions.append(SellAction(Position(0, 0)))
-        actions.append(BuildAction(TowerType.SPEAR_SHOOTER, Position(0, 0)))
+        curIndex = 0
+        curTeam = 0
 
-        if other_team_ids:
-            actions.append(SendReinforcementsAction(EnemyType.LVL1, other_team_ids[0]))
+        if gameMsg.teamInfos[ourTeamId].money >= 200:
+            pathPos: Position = gameMsg.map.paths[0].tiles[curIndex]
+            towerPos: Position = Position(pathPos.x + 1, pathPos.y + 1)
+            actions.append(BuildAction(TowerType.SPEAR_SHOOTER, towerPos))
+        else:
+            actions.append(SendReinforcementsAction(EnemyType.LVL1, otherTeamIds[curTeam % len(otherTeamIds)]))
+            curTeam += 1
 
         return actions
+
+    def getNeighbours(self, pos: Position, range: int):
+        neighbours = list()
+        for x in range(-range, range + 1):
+            for y in range(-range, range + 1):
+                if x == 0 and y == 0:
+                    continue
+
+                checkX = pos.x + x
+                checkY = pos.y + y
+                
+                if not self.isCellEmpty(self.gameMsg.teamId, Position(checkX, checkY)):
+                    continue
+
+                neighbours.append(Position(pos.x + x, pos.y + y))
+        return neighbours
+
+
+    def isCellOutOfBound(self, pos: Position):
+        if self.gameMsg.map.width >= pos.x or pos.x < 0:
+            return False
+        if self.gameMsg.map.height >= pos.y or pos.y < 0:
+            return False
+        
+        return True
+
+    def isCellEmpty(self, teamId, pos: Position):
+        if self.isCellOutOfBound(pos):
+            return False
+
+        cell = self.gameMsg.playAreas[teamId].grid[pos.x][pos.y]
+
+        return len(cell.enemies) == 0 and len(cell.towers) == 0 and len(cell.paths) == 0
