3ed5bb783bca8d9c358dab2feb6e205ea9b696db
William Fecteau <william.fecteau.1@ulaval.ca>
2023-01-14 09:56:32 -0500

    Cleanup

--- bot.py
+++ bot.py
@@ -8,32 +8,40 @@ class Bot:
         print("Initializing your super mega duper bot")
 
     def get_next_move(self, gameMsg: GameMessage):
-        """
-        Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
-        """
         self.gameMsg = gameMsg
 
         ourTeamId = gameMsg.teamId
-        otherTeamIds = [team for team in gameMsg.teams if team != gameMsg.teamId]
+        otherTeamIds = [
+            team for team in gameMsg.teams if team != gameMsg.teamId]
         actions = list()
-   
 
-        curIndex = 0
         curTeam = 0
 
         if gameMsg.teamInfos[ourTeamId].money >= 200:
-            pathPos: Position = gameMsg.map.paths[0].tiles[curIndex]
-            neighboursPositions = self.getNeighbours(pathPos, 1)
-            posIndex = random.randint(0, len(neighboursPositions) - 1)
+            buildAction: BuildAction = self.randomTowerPlacement()
 
-            towerPos = neighboursPositions[posIndex]
-            actions.append(BuildAction(TowerType.SPEAR_SHOOTER, towerPos))
-            curIndex += 1
+            actions.append(buildAction)
+            # pathPos: Position = gameMsg.map.paths[0].tiles[curIndex]
+            # neighboursPositions = self.getNeighbours(pathPos, 1)
+            # posIndex = random.randint(0, len(neighboursPositions) - 1)
+            # towerPos = neighboursPositions[posIndex]
         else:
-            actions.append(SendReinforcementsAction(EnemyType.LVL1, otherTeamIds[curTeam % len(otherTeamIds)]))
+            actions.append(SendReinforcementsAction(
+                EnemyType.LVL1, otherTeamIds[curTeam % len(otherTeamIds)]))
             curTeam += 1
 
         return actions
+
+    def randomTowerPlacement(self) -> BuildAction:
+        randX = random.randint(0, self.gameMsg.map.width - 1)
+        randY = random.randint(0, self.gameMsg.map.height - 1)
+
+        while not self.isCellEmpty(self.gameMsg.teamId, Position(randX, randY)):
+            randX = random.randint(0, self.gameMsg.map.width - 1)
+            randY = random.randint(0, self.gameMsg.map.height - 1)
+
+        towerPos = Position(randX, randY)
+        return BuildAction(TowerType.SPEAR_SHOOTER, towerPos)
 
     def getNeighbours(self, pos: Position, range: int):
         neighbours = list()
@@ -44,20 +52,20 @@ class Bot:
 
                 checkX = pos.x + x
                 checkY = pos.y + y
-                
+
                 if not self.isCellEmpty(self.gameMsg.teamId, Position(checkX, checkY)):
                     continue
 
-                neighbours.append(Position(pos.x + x, pos.y + y))
+                neighbours.append(Position(checkX, checkY))
+
         return neighbours
 
-
     def isCellOutOfBound(self, pos: Position):
-        if self.gameMsg.map.width >= pos.x or pos.x < 0:
+        if pos.x >= self.gameMsg.map.width or pos.x < 0:
             return False
-        if self.gameMsg.map.height >= pos.y or pos.y < 0:
+        if pos.y >= self.gameMsg.map.height or pos.y < 0:
             return False
-        
+
         return True
 
     def isCellEmpty(self, teamId, pos: Position):
