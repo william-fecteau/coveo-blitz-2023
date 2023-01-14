84852d36970b20f4d1eef2ade8c7984e4995527e
Zack Dufresne <zack.dufresne.1@ulaval.ca>
2023-01-14 09:20:46 -0500

    script fix

--- zipScript.py
+++ zipScript.py
@@ -1,9 +1,13 @@
 import zipfile
+from os import walk
+import pathlib
 
+myPath = pathlib.Path(__file__).parent.resolve()
 zippedFile = zipfile.ZipFile('code.zip', 'w')
 
-zippedFile.write('actions.py')
-zippedFile.write('application.py')
-zippedFile.write('bot.py')
-zippedFile.write('game_message.py')
+filenames = next(walk(myPath), (None, None, []))[2]
+for i in filenames:
+    if i != 'zipScript.py' and i != 'code.zip':
+        zippedFile.write(i)
+
 zippedFile.close()
